4b5f2b22157d233e228c78ad71706098b766c9df
maorka <maor@0525@gmail.com>
2022-07-31 17:59:47 +0300

    make idhospital and name uniqe columns   31/07/22-maor _

--- settings.py
+++ settings.py
@@ -52,11 +52,12 @@ except Error as e:
 
 #create tables
 dbCursor.execute("CREATE TABLE IF NOT EXISTS hospital ("
-                 "idhospital int(255)"
-                 ",Name varchar(45)"
+                 "idhospital int(255) NOT NULL"
+                 ",Name varchar(45) NOT NULL"
                  ",Fixed_activity_level float"
                  ",Transport_time float,"
-                 "deleted BOOLEAN)");
+                 "deleted BOOLEAN,"
+                 "PRIMARY KEY(idhospital,Name))");
 
 dbCursor.execute("CREATE TABLE IF NOT EXISTS resourcecyclotron ("
                  "idresourceCyclotron int(255)"
@@ -162,7 +163,7 @@ hospitals_in_db = cursor.fetchall();
 
 #Insert data of Hospitals into My-SQl DB
 #The INSERT IGNORE statement will cause MySQL to do nothing when the insertion throws an error. If there’s no error, then a new row will be added to the table.
-cursor.execute("INSERT IGNORE INTO hospital (idhospital,Name,Fixed_activity_level,Transport_time) VALUES (1,'Belinson',9.2,15.0),(2,'Ichilov',10.0,20.0),(3,'Assuta TA',10.9,30.0),(4,'Sheb',10.5,35.0),(5,'Ziv',11.0,25.0),(6,'Assuta Ashdod',13.1,60.0),(7,'Assaf Harofeh',10.6,65.0),(8,'Augusta Victoria',9.6,50.0),(9,'Hila Pharma',9.6,50.0),(10,'Hadassah',9.5,0.0);")
+cursor.execute("INSERT IGNORE INTO hospital (idhospital,Name,Fixed_activity_level,Transport_time,deleted) VALUES (1,'Belinson',9.2,15.0,true),(2,'Ichilov',10.0,20.0,false),(3,'Assuta TA',10.9,30.0,true),(4,'Sheb',10.5,35.0,true),(5,'Ziv',11.0,25.0,true),(6,'Assuta Ashdod',13.1,60.0,false),(7,'Assaf Harofeh',10.6,65.0,false),(8,'Augusta Victoria',9.6,50.0,false),(9,'Hila Pharma',9.6,50.0,false),(10,'Hadassah',9.5,0.0,false);")
 #cleanup DB
 db.commit();
 # cursor.close();
