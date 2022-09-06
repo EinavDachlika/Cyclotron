e226ba8eab379fc681a92028c780ff8fea6bdb60
Einav <einavda@gmail.com>
2022-09-06 14:38:12 +0300

    bdika

--- ConnectToDB.py
+++ ConnectToDB.py
@@ -3,15 +3,15 @@ from mysql.connector import Error
 
 # connect to MySqL
 try:
-    #SRY local DB Mysql
-    db = mysql.connector.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        password="root",
-        database="cyclotron",
-        auth_plugin='mysql_native_password'
-    )
+    # #SRY local DB Mysql
+    # db = mysql.connector.connect(
+    #     host="localhost",
+    #     port=3306,
+    #     user="root",
+    #     password="root",
+    #     database="cyclotron",
+    #     auth_plugin='mysql_native_password'
+    # )
 
     #
     #Maor local DB Mysql
@@ -22,12 +22,12 @@ try:
     #     password="root",
     #     database="cyclotron")
 
-    # # # Einav local DB-Mysql
-    # db = mysql.connector.connect(
-    #     host="localhost",
-    #     user="root",
-    #     password="Cyclotron2022@?%",
-    #     database= "cyclotron")
+    # # Einav local DB-Mysql
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Cyclotron2022@?%",
+        database= "cyclotron")
 
     if db.is_connected():
         # db_Info = db.get_server_info()
--- main.py
+++ main.py
@@ -2,28 +2,34 @@
 import sys,importlib
 import Permission
 
+# def importAdmin():
+#     sys.path.append("./UserPages.py")
+
 def importAdmin():
-    spec = importlib.util.spec_from_file_location("module.name", "D:\PythonProjects\Cyclotron\‏‏AdminPages.py")
+    spec = importlib.util.spec_from_file_location("module.name", r"‏‏AdminPages.py")
     foo = importlib.util.module_from_spec(spec)
     sys.modules["module.name"] = foo
     spec.loader.exec_module(foo)
+#foo.importAdmin();#call to admin pages function
 
-def importUser():
-    spec = importlib.util.spec_from_file_location("module.name", "D:/PythonProjects/Cyclotron/UserPages.py")
-    foo = importlib.util.module_from_spec(spec)
-    sys.modules["module.name"] = foo
-    spec.loader.exec_module(foo)
+# def importUser():
+#     spec = importlib.util.spec_from_file_location("module.name", "./UserPages.py")
+#     foo = importlib.util.module_from_spec(spec)
+#     sys.modules["module.name"] = foo
+#     spec.loader.exec_module(foo)
 
-if ((Permission.user_verified) and (Permission.password_verfied) and (Permission.ValidateTypeOfUser=='admin')):
-    print("Login successful-Admin");
-    #root.destroy();
-    importAdmin();#call to admin pages function
+# if ((Permission.user_verified) and (Permission.password_verfied) and (Permission.ValidateTypeOfUser=='admin')):
+#     print("Login successful-Admin");
+#     #root.destroy();
+# importAdmin();#call to admin pages function
     #root.deiconify();
 
-elif ((Permission.user_verified) and (Permission.password_verfied) and (Permission.ValidateTypeOfUser=='user')):
-    print("Login successful-User");
-    importUser();
-    #root.destroy();
-    #importUser();
-    #root.deiconify();
-
+# elif ((Permission.user_verified) and (Permission.password_verfied) and (Permission.ValidateTypeOfUser=='user')):
+#     print("Login successful-User");
+#     importAdmin();
+#     #root.destroy();
+#     #importUser();
+#     #root.deiconify();
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    importAdmin()
